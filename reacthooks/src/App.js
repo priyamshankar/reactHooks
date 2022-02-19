be501f009c0862e9df24167a551dca8b0585b7ac
priyam <priyamshankar.5@gmail.com>
2022-02-19 22:34:03 +0530

    basic react hooks

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
 
-function App() {
+
+const App = () => {
+  const [myName,setmyName]=useState("my name is priyam shankar");
+  const changeName=()=>{
+  
+  setmyName("my name is really priyam shankar...");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>{myName}</h1>
+    <button onClick={changeName}>click here</button>
+    
+    </>
+  )
 }
 
-export default App;
+export default App
