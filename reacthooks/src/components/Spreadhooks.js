00b0ba40e3619a09ae071a4d36b71e3fb0f7a906
priyam <priyamshankar.5@gmail.com>
2022-07-13 01:51:00 +0530

    practice ract

--- reacthooks/src/components/Spreadhooks.js
+++ reacthooks/src/components/Spreadhooks.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react'
 
 const Spreadhooks = () => {
     const [spreadIt,setspreadIt]=useState({
-        name:"priyam shnankar", regNo:"20BCE10032", ekfield:"kuch bhi"
+        name:"priyam shankar", regNo:"20BCE10032", ekfield:"kuch bhi"
     })
 const clickIt=()=>{
-    setspreadIt({...spreadIt,ekfield:"kuch aur bhi"});
+    setspreadIt({...spreadIt,ekfield:"kuch aur bhi",regNo:"20BAI10032"});
+    // setspreadIt({...spreadIt,regNo:"20BAI10032"});
 }
   return (
     <>
     <h1>
     {spreadIt.ekfield}
+    <br />
+    {spreadIt.regNo}
     </h1>
 
-    <button onClick={clickIt}>chane karo</button>
+    <button onClick={clickIt}>change karo</button>
     </>
   )
 }
